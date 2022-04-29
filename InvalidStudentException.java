ffa1208d185341a839b0decf0280295af2d3f9c4
Sumanth Reddy <42690123+Sumanth29@users.noreply.github.com>
2022-04-29 15:36:30 +0530

    Add files via upload

---
+++ InvalidStudentException.java
@@ -0,0 +1,11 @@
+// ELearningApp/src/com/exception/InvalidStudentException.java  
+ package com.exception;
+
+ public class InvalidStudentException extends Exception {
+ 
+
+	public InvalidStudentException(String msg){
+	super(msg); 	}
+
+ }
+
---
+++ StudentDAO.java
@@ -0,0 +1,33 @@
+// ELearningApp/src/com/dao/StudentDAO.java   
+ package com.dao;
+
+	import java.util.ArrayList;
+	import java.util.List;
+
+	import com.exception.InvalidStudentException;
+	import com.model.Student;
+
+ public class StudentDAO { 
+	List<Student> studentList = new ArrayList<>(); 
+	public void addStudent(Student studentObj){
+	studentList.add(studentObj); 	}
+
+	public Student viewStudentById(int studentid) throws InvalidStudentException{
+	if(studentList.isEmpty())
+	{
+		throw new InvalidStudentException("Student list is empty"); 	}
+	else
+	{
+	
+
+	for(Student s : studentList)
+	{
+	if(s.getStudentId().equals(studentid))
+	{
+	return s;
+	}
+	}
+	}
+		return null; 	}
+ }
+
